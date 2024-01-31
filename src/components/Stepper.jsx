105324ce5e29e7b7955548db0cc23b9dbd6b7dfb
Talha Ghouri <talhaghouridev@gmail.com>
2024-01-31 11:58:14 -0800

    Add the stepper compoent add fix the styles

--- src/components/Stepper.jsx
+++ src/components/Stepper.jsx
@@ -1,49 +1,46 @@
 import React from "react";
 import { STEPPER_STEPS } from "@constants/index";
 
-const Stepper = (activeStep=1) => {
+const Stepper = (activeStep = 1) => {
   return (
-    <div id="stepper" key={activeStep} className="relative">
-      <div className="max-w-1000 mx-auto flex gap-20 px-[15px] stepper_container md:gap-0">
+    <div id="stepper" key={activeStep} className="mt-[30px]" >
+      <div className="stepper_container container font-SansBold items-center justify-center max-w-[900px] mx-auto flex gap-[20px] px-[40px] md:gap-0 md:p-[15px]">
         {STEPPER_STEPS?.map((step, index) => {
           const { icon: Icon, label } = step;
           return (
             <>
               <div
-                className={`flex items-center justify-start w-full max-w-[fit-content] step ${
+                className={` step w-full max-w-fit flex items-center justify-center gap-[6px] ${
                   activeStep === index ? "stepActive" : ""
                 }`}
                 key={index}
               >
+                <Icon className="text-[22px]" />
                 <h1
-                  className={`flex items-center justify-center gap-[5px] color-${
-                    activeStep >= index ? "#d23f57" : "00000099"
-                  } md:flex-col`}
+                  className={`flex items-center justify-center gap-[6px] ${
+                    activeStep >= index ? "text-rose-500" : "text-gray-600"
+                  }`}
                 >
-                  <Icon />
-
-                  <h1>{step.label}</h1>
-                  <p
-                    className={`text-center md:text-xs ${
-                      activeStep >= index
-                        ? "text-[11px] max-w-[min-content]"
-                        : ""
-                    }`}
-                  >
-                    {/* {step.description} */}
-                  </p>
+                  {step.label}
+                  {activeStep >= index && (
+                    <p className="text-center md:text-xs md:max-w-min-content">
+                      {/* Add your content for active step */}
+                    </p>
+                  )}
                 </h1>
               </div>
               <div
-                className={`flex items-center justify-center w-full step_line ${
-                  activeStep === index ? "stepLineActive" : ""
+                className={`w-full flex items-center justify-center ${
+                  activeStep === index ? "bg-[#d23f57]" : ""
                 }`}
                 style={{ display: index === 2 ? "none" : "" }}
               >
                 <span
-                  className={`h-[1.5px] w-full bg-${
-                    activeStep >= index ? "#d23f57" : "0000004f"
-                  } flex rounded-[5px]`}
+                  className="h-[1.5px] w-full bg-gray-400 rounded-[5px]"
+                  style={{
+                    backgroundColor:
+                      activeStep >= index ? "#d23f57" : "",
+                  }}
                 ></span>
               </div>
             </>
